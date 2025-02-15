b87fdd875c3b9c7d89ebf72dae9d30b1d6549f5d
Владимир Манченко <vladimirmancenko@r.local>
2025-02-15 12:57:32 +0300

    Обновление script.js

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+function pauseAnimation() {
+    console.log("Pause animation called");
+}
+
+function resumeAnimation() {
+    console.log("Resume animation called");
+}
+
+function submitIdea() {
+    console.log("Submit idea called");
+}
+
+function loginAdmin() {
+    console.log("Login admin called");
+}
